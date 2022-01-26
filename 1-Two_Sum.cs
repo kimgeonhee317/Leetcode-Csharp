2fbcf297b67a6299ca3aa27a047660004ae1367d
geon <rlarjsgml317@gmail.com>
2022-01-26 16:02:52 +0900

    commit 1, 53
    post completed

---
+++ 1-Two_Sum.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int left;
+        int[] answer = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            left = target - nums[i];
+            int index = Array.IndexOf(nums, left);
+
+            if (i == index)
+            {
+                continue;
+            }
+
+            if (index != -1)
+            {
+                answer[0] = i;
+                answer[1] = index;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ 53-Maximum_Subarray.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int[] local_maximum = new int[nums.Length];
+        int global_maximum;
+
+        global_maximum = nums[0];
+        local_maximum[0] = nums[0];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i != 0)
+            {
+                local_maximum[i] = local_maximum[i - 1] + nums[i];
+            }
+
+            if (local_maximum[i] > global_maximum)
+            {
+                global_maximum = local_maximum[i];
+            }
+
+            if (local_maximum[i] < 0)
+            {
+                local_maximum[i] = 0;
+            }
+        }
+
+        return global_maximum;
+    }
+
+}
