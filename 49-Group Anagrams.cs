df41b824aa991d1a1d513da5c24ec53dc51067e1
geon <rlarjsgml317@gmail.com>
2022-01-27 11:05:52 +0900

    Create 49-Group Anagrams.cs
    solved 49

---
+++ 49-Group Anagrams.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        IList<IList<string>> answerList = new List<IList<string>>();
+        Dictionary<string, List<string>> hashtable = new Dictionary<string, List<string>>();
+
+        foreach (string str in strs)
+        {
+            string key = new string(str.OrderBy(x => x).ToArray()); //Get key by sorting
+
+            if (!hashtable.ContainsKey(key)) // Add key and value if there is no key
+            {
+                hashtable.Add(key, new List<string>());
+                hashtable[key].Add(str);
+            }
+            else // Add value to list if there is key already
+            {
+                hashtable[key].Add(str);
+            }
+        }
+
+        foreach (string key in hashtable.Keys)
+        {
+            answerList.Add(hashtable[key]);
+        }
+
+        return (IList<IList<string>>)answerList;
+    }
+}
