71b9a3175dce4b88f471bd4d34aacd0ce7e80267
geon <rlarjsgml317@gmail.com>
2022-01-27 16:06:08 +0900

    62, 94
    solved 62, 94

---
+++ 62-Unique_Paths.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int UniquePaths(int m, int n)
+    {
+        int[,] memo = new int[m, n];
+
+        makeMemo(memo, m, n); //construct dp map
+
+        return memo[m - 1, n - 1];
+    }
+
+    public void makeMemo(int[,] memo, int m, int n)
+    {
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    memo[i, j] = 1;
+                    continue;
+                }
+                memo[i, j] = memo[i - 1, j] + memo[i, j - 1];
+            }
+        }
+    }
+}
---
+++ 94-Binary Tree Inorder Traversal.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> list = new List<int>();
+        traversal(list, root);
+
+        //foreach (int item in list)
+        //{
+        //    console.writeline(item);
+        //}
+
+
+        return list;
+    }
+
+    public void traversal(List<int> list, TreeNode node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        traversal(list, node.left);
+        list.Add(node.val);
+        traversal(list, node.right);
+    }
+}
